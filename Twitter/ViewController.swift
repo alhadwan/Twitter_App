2122c451f0cc033b613c401eb28acc4e4a929ac2
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2016-02-14 00:21:17 -0800

    editd MovieView

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Ali Hadwan on 2/9/16.
+//  Copyright © 2016 Ali Hadwan. All rights reserved.
+//
+
+
+import UIKit
+import BDBOAuth1Manager
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onLogin(sender: AnyObject) {
+
+        
+        TwitterClient.sharedInstance.loginWithCompletion() {
+            (user: User?, error: NSError?) in
+            if user != nil {
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+            } else {
+                // handle error
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    //        TwitterClient.sharedInstance.loginWithBlock() {
+    //            // go to next screen
+    //
+    //        }
+
+}
+
